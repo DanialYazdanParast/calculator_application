463a28ccacfa93bebb8031062c67371b4e4d3489
Danial YazdanParast <danyal.y0098@gmail.com>
2024-03-08 18:47:17 +0330

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  var inputUser = '';
+  var result = '';
+  void buttonPressed(String text) {
+    setState(() {
+      inputUser = inputUser + text;
+    });
+  }
+
+  Widget getRow(String text1, String text2, String text3, String text4) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      TextButton(
+        style: TextButton.styleFrom(
+            shape: CircleBorder(
+              side: BorderSide(width: 0, color: Colors.transparent),
+            ),
+            backgroundColor: getBackgroundColor(text1)),
+        onPressed: () {
+          if (text1 == 'ac') {
+            setState(() {
+              inputUser = '';
+              result = '';
+            });
+          } else {
+            buttonPressed(text1);
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(3),
+          child: Text(
+            '$text1',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26, color: getColortext(text1)),
+          ),
+        ),
+      ),
+      TextButton(
+        style: TextButton.styleFrom(
+            shape: CircleBorder(
+              side: BorderSide(width: 0, color: Colors.transparent),
+            ),
+            backgroundColor: getBackgroundColor(text2)),
+        onPressed: () {
+          if (text2 == 'ce') {
+            setState(() {
+              if (inputUser.length > 0) {
+                inputUser = inputUser.substring(0, inputUser.length - 1);
+              }
+            });
+          } else {
+            buttonPressed(text2);
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(3),
+          child: Text(
+            '$text2',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26, color: getColortext(text2)),
+          ),
+        ),
+      ),
+      TextButton(
+        style: TextButton.styleFrom(
+            shape: CircleBorder(
+              side: BorderSide(width: 0, color: Colors.transparent),
+            ),
+            backgroundColor: getBackgroundColor(text3)),
+        onPressed: () {
+          buttonPressed(text3);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(3),
+          child: Text(
+            '$text3',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26, color: getColortext(text3)),
+          ),
+        ),
+      ),
+      TextButton(
+        style: TextButton.styleFrom(
+            shape: CircleBorder(
+              side: BorderSide(width: 0, color: Colors.transparent),
+            ),
+            backgroundColor: getBackgroundColor(text4)),
+        onPressed: () {
+          if (text4 == '=') {
+            Parser parser = Parser();
+            Expression expression = parser.parse(inputUser);
+            ContextModel contextModel = ContextModel();
+            double eval =
+                expression.evaluate(EvaluationType.REAL, contextModel);
+            setState(() {
+              result = eval.toString();
+            });
+          } else {
+            buttonPressed(text4);
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(3),
+          child: Text(
+            '$text4',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 26, color: getColortext(text4)),
+          ),
+        ),
+      ),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+            child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: backgroundGreyDark,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          '$inputUser',
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                              color: textGreen,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          '$result',
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                              color: textGrey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 62),
+                        ),
+                      ),
+                    ]),
+              ),
+              flex: 3,
+            ),
+            Expanded(
+              child: Container(
+                color: backgroundGrey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    getRow('ac', 'ce', '%', '/'),
+                    getRow('7', '8', '9', '*'),
+                    getRow('4', '5', '6', '-'),
+                    getRow('1', '2', '3', '+'),
+                    getRow('00', '0', '.', '='),
+                  ],
+                ),
+              ),
+              flex: 7,
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+
+  bool isOperator(String text) {
+    var list = ['ac', 'ce', '%', '/', '*', '-', '+', '='];
+    for (var item in list) {
+      if (text == item) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  Color getBackgroundColor(String text) {
+    if (isOperator(text)) {
+      return backgroundGreyDark;
+    } else {
+      return backgroundGrey;
+    }
+  }
+
+  Color getColortext(String text) {
+    if (isOperator(text)) {
+      return textGreen;
+    } else {
+      return textGrey;
+    }
+  }
+}
